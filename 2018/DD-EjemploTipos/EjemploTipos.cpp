291f240630fd2c537e9a2ec5f46be9969ab84963
juanmanuelfrei <jfrei@net2one.com.ar>
2018-04-15 15:11:38 -0300

    Add files via upload

---
+++ 2018/DD-EjemploTipos/EjemploTipos.cpp
@@ -0,0 +1,32 @@
+/*
+	Ejercicio 2
+	"Ejemplotipos"
+	uan Manuel Bottarelli Frei
+	K1051
+*/
+
+#include <iostream>
+#include <assert.h> 
+
+int main()
+{
+	
+	std::cout << "Ejemplo de variables en C++\n\n";
+	
+	std::cout << "Las variables en C++ son las siguientes:\n"; 
+	std::cout << "1- Para variables BOOL\n";
+	std::cout << "2- Para variables CHAR\n";
+	std::cout << "3- Para variables UNSIGNED\n";
+	std::cout << "4- Para variables INT\n";
+	std::cout << "5- Para variables DOUBLE\n";
+	std::cout << "6- Para variables STRING\n \n";
+	
+	std::cout << "Ejemplos:\n";
+	std::cout << "1- Almacena 1 o 0 dependiendo si la variable es True(1) o False(0)\n";
+	std::cout << "2- Almacena caracteres: C, c, A, a, B, c\n";
+	std::cout << "3- Almacena numeros positivos y el 0: 0, 1, 2, 3, 4, 5\n";
+	std::cout << "4- Amacena números enteros: 15, -1, 0, -20, 45\n";
+	std::cout << "5- Almacena números reales: 8.999999, 3.1452\n";
+	std::cout << "6- Almacena una cadena de caracteres: 'Hola Mundo', 'Hello Wordl'\n";
+
+}
