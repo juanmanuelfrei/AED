99337e6fbe499f01b5abf9c137fa3d0bda77e121
juanmanuelfrei <jfrei@net2one.com.ar>
2018-04-15 14:01:56 -0300

    Add files via upload

---
+++ 2018/DD-Adicion/Adicion.cpp
@@ -0,0 +1,16 @@
+/*
+	Ejercicio 1
+	Adicion
+	Juan Manuel Bottarelli Frei
+	K1051
+*/
+
+#include <iostream>
+
+int main ()
+{
+	int a, b;
+	std:: cout << "Ingrese los valores de A y B los cuales seran sumados\n";
+	std:: cin >> a >> b;
+	std:: cout << "Suma de A y B:" << a+b;
+}
